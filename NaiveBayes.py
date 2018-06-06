15b1a60c3fe666c7deacc1867dd42a9a4944b62d
Mayank <mayank.trp48@gmail.com>
2018-06-07 00:29:03 +0530

    knn

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #assigning predictor and target variables
 x= np.array([[-3,7],[1,5], [1,2], [-2,0], [2,3], [-4,0], [-1,1], [1,1], [-2,2], [2,7], [-4,1], [-2,7]])
-y = np.array([3, 3, 3, 3, 4, 3, 3, 4, 3, 4, 4, 4])
+y = np.array([3, 5, 3, 5, 4, 3, 3, 4, 3, 4, 4, 4])
 
 
 model = GaussianNB()
@@ -12,5 +12,5 @@ model = GaussianNB()
 model.fit(x, y)
 
 #Predict Output 
-predicted= model.predict([[1,2],[3,4]])
+predicted= model.predict([[1,2]])
 print (predicted)
